744526270f0075838e1d112cc636376290b03818
lahmer ilyas <ilyaslahmer93@gmail.com>
2021-10-09 11:34:55 +0100

    load fonts

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as Font from 'expo-font';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { MainApp } from './src/navigation';
 
+// Define all your fonts below
+const customFonts = {
+  // SFProTextBold: require('./assets/fonts/SF-Pro-Text-Bold.ttf'),
+};
 export default function App() {
+  const [loaded, setLoaded] = useState<boolean>(false);
+  useEffect(() => {
+    Font.loadAsync(customFonts)
+      .then(() => setLoaded(true))
+      .catch((e) => console.log(JSON.stringify(e)));
+  }, []);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <MainApp />
+      {!loaded && <Text>Loading Assets</Text>}
+      {loaded && <MainApp />}
     </View>
   );
 }
